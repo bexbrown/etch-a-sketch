fe1c8f72158745714e778acc374096147b98f516
Rebecca Brown <rebecca.brown@msn.com>
2022-04-03 19:23:19 -0400

    made gameboard size responsive to user input

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ board.style.border = "black solid 1px";
 
 
 function createTiles() {
+    //insert number in place of 256
     for (let i = 0; i < 256; i++) {
         let tile = document.createElement("div");
         tile.classList.add("tile");
@@ -22,15 +23,40 @@ function numberOfTiles(result) {
 }
 
 
+
+
 let clearButton = document.querySelector("button");
 
 clearButton.addEventListener("click", clearButtonClick);
 
-let result = window.prompt("Pick a number between 1 and 100", 16);
 
 function clearButtonClick() {
-    window.prompt("Pick a number between 1 and 100", 16);
-}
 
+    let result = window.prompt("Pick a number between 1 and 100");
+    console.log(result)
+    let totalTiles = result * result;
+    console.log(totalTiles);
+    tileLength = 320 / result;
+    console.log(tileLength)
+
+    function removeAllChildNodes(parent) {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
+    }
+    removeAllChildNodes(board);
+
+    function createNewTiles(totalTiles) {
+        for (let i = 0; i < totalTiles; i++) {
+            let tile = document.createElement("div");
+            tile.classList.add("tile");
+            board.appendChild(tile);
+            tile.style.length = tileLength + "px";
+            tile.style.width = tileLength + "px";
+
+        }
+    }
+    createNewTiles(totalTiles);
+};
 
 
