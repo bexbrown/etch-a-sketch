fdb68e15d208d83bbdce9419297199687ff484d8
Rebecca Brown <rebecca.brown@msn.com>
2022-04-03 20:50:43 -0400

    reprompt user if imput is out side of parameters of 0 and 100

--- script.js
+++ script.js
@@ -34,38 +34,46 @@ clearButton.addEventListener("click", clearButtonClick);
 function clearButtonClick() {
 
     let result = window.prompt("Pick a number between 1 and 100");
-    console.log(result)
-    let totalTiles = result * result;
-    console.log(totalTiles);
-    tileLength = 320 / result;
-    console.log(tileLength)
 
-    function removeAllChildNodes(parent) {
-        while (parent.firstChild) {
-            parent.removeChild(parent.firstChild);
+
+    if (result >= 0 && result <= 100) {
+        console.log(result)
+        let totalTiles = result * result;
+        console.log(totalTiles);
+        tileLength = 320 / result;
+        console.log(tileLength)
+
+        function removeAllChildNodes(parent) {
+            while (parent.firstChild) {
+                parent.removeChild(parent.firstChild);
+            }
         }
+        removeAllChildNodes(board);
+
+        function createNewTiles(totalTiles) {
+            for (let i = 0; i < totalTiles; i++) {
+                let tile = document.createElement("div");
+                tile.classList.add("tile");
+                board.appendChild(tile);
+                tile.style.length = tileLength + "px";
+                tile.style.width = tileLength + "px";
+
+                tile.addEventListener("mouseover", function () {
+                    tile.style.backgroundColor = "black";
+                })
+                tile.addEventListener("mouseout", function () {
+                    tile.style.opacity = 0;
+                    tile.style.transition = "opacity 3s linear";
+                })
+            }
+        };
+
+        createNewTiles(totalTiles);
+    } else {
+        clearButtonClick();
     }
-    removeAllChildNodes(board);
 
-    function createNewTiles(totalTiles) {
-        for (let i = 0; i < totalTiles; i++) {
-            let tile = document.createElement("div");
-            tile.classList.add("tile");
-            board.appendChild(tile);
-            tile.style.length = tileLength + "px";
-            tile.style.width = tileLength + "px";
 
-            tile.addEventListener("mouseover", function () {
-                tile.style.backgroundColor = "black";
-            })
-            tile.addEventListener("mouseout", function () {
-                tile.style.opacity = 0;
-                tile.style.transition = "opacity 3s linear";
-            })
-        }
-    };
-
-    createNewTiles(totalTiles);
 };
 
 
